fe78f28dd8c1b2027b9fd25d038e478ac5b2b471
Ray <zruiwh@cn.ibm.com>
2017-10-11 15:22:05 +0800

    change appid

--- config.js
+++ config.js
@@ -21,17 +21,17 @@ export default {
 		path: 'public/uploads/'
 	},
 	superAdmin: {
-		username: 'admin', 
-		password: '123456', 
+		username: 'admin',
+		password: '123456',
 	},
 	orderStatus: {
-		'submitted': '已提交', 
-		'canceled' : '已取消', 
-		'confirmed': '已确认', 
-		'finished' : '已完成', 
+		'submitted': '已提交',
+		'canceled' : '已取消',
+		'confirmed': '已确认',
+		'finished' : '已完成',
 	},
 	wechat: {
-		appid: 'wx4ec54c44008298ec', 
-		secret: '8295e6dd753aab17624e0bb0e1c4e98d', 
+		appid: 'wxacb797a7e31213fe',
+		secret: 'f915ef61c2f50c5a097c3db159c4fe6c',
 	},
 }
