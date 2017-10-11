a3823b1a70a446c524dc1f5d76406be8234a6a75
Ray <zruiwh@cn.ibm.com>
2017-10-11 14:24:52 +0800

    disable redis auth

--- db/redis.js
+++ db/redis.js
@@ -2,13 +2,26 @@ import redis from 'redis'
 import config from '../config'
 
 const redisLink = config['redis'][process.env.NODE_ENV || 'development']['connectionString']
-const redisClient = redis.createClient(redisLink)
+// const redisClient = redis.createClient(redisLink)
+//
+// redisClient
+// 	.on('error', err => console.log('------ Redis connection failed ------' + err))
+// 	.on('connect', () => console.log('------ Redis connection succeed ------'))
 
-redisClient
-	.on('error', err => console.log('------ Redis connection failed ------' + err))
-	.on('connect', () => console.log('------ Redis connection succeed ------'))
+const redisClient = {
+	get:(token,cb) => {
+		cb(null,null)
+	},
 
+	set:() => {
+
+	},
+
+	expire:() =>{
+
+	}
+}
 export default {
-	redis: redis, 
-	redisClient: redisClient, 
+	redis: redis,
+	redisClient: redisClient,
 }
